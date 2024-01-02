86c8bf3cfd79c67674a8b7af8aa02bceb1237621
Andor Kesselman <andor@magic-pro-3.local>
2024-01-02 08:26:35 -0800

    added build makefile

---
+++ apps/RNBridgeSample/ios/RNBridgeSample.m
@@ -0,0 +1,10 @@
+//
+//  RNBridgeSampleCFile.m
+//  RNBridgeSample
+//
+//  Created by Andor Kesselman on 1/2/24.
+//
+#import "React/RCTBridgeModule.h"
+@interface RCT_EXTERN_MODULE(RNBridgeSample, NSObject)
+RCT_EXTERN_METHOD(increment)
+@end
